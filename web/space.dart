bd4951c5046c903b141d805c2d54c7e1c4b3799b
charxie <charxie@gmail.com>
2014-08-06 09:07:13 -0400

    action class

--- web/space.dart
+++ web/space.dart
@@ -3,10 +3,13 @@ library space;
 import 'dart:convert';
 import 'dart:html';
 
+part 'action.dart';
+
 final RegExp whitespacePattern = new RegExp(r"(\s+)");
 
 CanvasElement canvas = querySelector("#canvas1");
 String dataFile = "energy3d.json";
+List<Action> actions = new List<Action>();
 
 void main() {
   
@@ -19,8 +22,14 @@ void _onDataLoaded(String responseText) {
   Map map = JSON.decode(responseText);
 
   int n = map["Activities"].length;
-  
-  print(n);
+
+  for(int i = 0; i < n; i++) {
+    Map x = map["Activities"][i];
+    if(x.containsKey("Add Wall")) {
+      Action a = new Action(Action.ADD_WALL, x["Timestamp"], x["File"], x["Add Wall"].toString());
+      print(a.toString());
+    }
+  }
   
   
 }
