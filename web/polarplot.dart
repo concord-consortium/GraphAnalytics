8a8fe9487b1ce47b348da96843d0b49a1a7d69b3
charxie <charxie@gmail.com>
2014-08-10 19:58:24 -0400

    polar graph

--- web/polarplot.dart
+++ web/polarplot.dart
@@ -10,18 +10,33 @@ class PolarPlot {
   int dimension = 10;
   List<Action> actions;
 
-  int _innerCircleRadius = 20;
+  double _symbolSize = 2.0;
+  int _innerCircleRadius = 30;
   int _arrowHeadSize = 10;
   bool _drag = false;
   num _dragStartPointX, _dragStartPointY;
+  Map<String, int> _actionMap;
 
 
   PolarPlot(this.canvas, this.actions) {
+    _analyzeActions();
     context = canvas.context2D;
     canvas.onMouseMove.listen(_onMouseMove);
     canvas.onMouseDown.listen(_onMouseDown);
     canvas.onMouseUp.listen(_onMouseUp);
     canvas.onMouseLeave.listen(_onMouseOut);
+  }
+  
+  void _analyzeActions() {
+    _actionMap = new Map<String, int>();
+    for(Action a in actions) {
+      if(_actionMap.containsKey(a.type)) {
+        _actionMap[a.type] ++;        
+      } else {
+        _actionMap.putIfAbsent(a.type, ()=>1);
+      }
+    }
+    dimension = _actionMap.length;
   }
   
   void _onMouseOut(MouseEvent e) {
@@ -67,10 +82,24 @@ class PolarPlot {
 
     double axisLength = canvas.width / 2 - 10;
     double delta = 2 * Math.PI / dimension;
-    for(int i = 0; i < dimension; i++){
-      _drawLineWithArrowHead(xc, yc, xc + axisLength * Math.cos(delta * i), yc + axisLength * Math.sin(delta * i), 1, "black");
+    double angle;
+    for(int i = 0; i < dimension; i++) {
+      angle = i * delta;
+      _drawLineWithArrowHead(xc, yc, xc + axisLength * Math.cos(angle), yc + axisLength * Math.sin(angle), 1, "black", i);
     }
-  
+
+    context.translate(xc, yc);
+    for(int i = 0; i < dimension; i++) {
+      angle = i * delta;
+      context..save()
+             ..rotate(angle)
+             ..font = "8pt Arial"
+             ..textAlign = "left"
+             ..fillText(_actionMap.keys.elementAt(i), _innerCircleRadius + 100, -4)
+             ..restore();
+    }
+    context.translate(-xc, -yc);
+
     _drawTitle();
 
   }
@@ -100,7 +129,8 @@ class PolarPlot {
            ..closePath();
   }
 
-  void _drawLineWithArrowHead(num x1, num y1, num x2, num y2, int weight, String color) {
+  void _drawLineWithArrowHead(num x1, num y1, num x2, num y2, int weight, String color, int index) {
+
     _drawLine(x1, y1, x2, y2, weight, color);
     num dx = x2 - x1;
     num dy = y2 - y1;
@@ -113,6 +143,14 @@ class PolarPlot {
     wingx = _arrowHeadSize * (arrowx * COS - arrowy * SIN);
     wingy = _arrowHeadSize * (arrowy * COS + arrowx * SIN);
     _drawLine(x2, y2, x2 - wingx, y2 - wingy, weight, color);
+    
+    int n = _actionMap.values.elementAt(index);
+    double p = 2 * _symbolSize + 1;
+    for(int i = 0; i < n; i++) {
+      double q = _innerCircleRadius + (i + 1) * p;
+      _drawCircle(x1 + q * arrowx, y1 + q * arrowy, _symbolSize, 1, "black");
+    }
+    
   }
 
 }
